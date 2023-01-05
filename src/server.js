81ccb288932551568fba58835d0b86c524ec8754
shonrob <rob19063@byui.edu>
2023-01-05 16:16:11 -0700

    update files to hopefully get render working

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use('/', require('../routes/index'))
+app.use('/', require('./routes/index'))
 
 app.listen(port,() => {
     console.log(`Running on port ${port}`)
